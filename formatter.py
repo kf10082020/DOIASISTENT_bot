324ee05707415f2fd7159cc5f9ea4956ea91b64c
kf10082020 <kf10082020@gmail.com>
2025-06-12 14:34:43 +0300

    Add files via upload

--- formatter.py
+++ formatter.py
@@ -1,1 +1,7 @@
-# Форматирует данные в удобный для Telegram вид
+
+def format_reply(metadata: dict) -> str:
+    return f"*Название:* {metadata['title']}\n" \
+           f"*Авторы:* {", ".join(metadata['authors'])}\n" \
+           f"*Журнал:* {metadata['journal']}\n" \
+           f"*Год:* {metadata['year']}\n" \
+           f"*DOI:* {metadata['doi']}"
