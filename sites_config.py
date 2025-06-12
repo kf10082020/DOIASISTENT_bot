84abb74830c2806279a280182331d812b48a690d
kf10082020 <kf10082020@gmail.com>
2025-06-12 17:59:20 +0300

    Update sites_config.py

--- sites_config.py
+++ sites_config.py
@@ -1,62 +1,23 @@
 def parse_mdpi(url): return {"title": "MDPI parser", "authors": "—", "journal": "MDPI", "issued": "—"}
-
-def parse_springer(url):
-    return {"title": "Springer parser (заглушка)", "authors": "—", "journal": "Springer", "issued": "—"}
-
-def parse_sciencedirect(url):
-    return {"title": "Sciencedirect parser (заглушка)", "authors": "—", "journal": "Sciencedirect", "issued": "—"}
-
-def parse_sagepub(url):
-    return {"title": "Sagepub parser (заглушка)", "authors": "—", "journal": "Sagepub", "issued": "—"}
-
-def parse_tandfonline(url):
-    return {"title": "Tandfonline parser (заглушка)", "authors": "—", "journal": "Tandfonline", "issued": "—"}
-
-def parse_bmcmedicine(url):
-    return {"title": "Bmcmedicine parser (заглушка)", "authors": "—", "journal": "Bmcmedicine", "issued": "—"}
-
-def parse_frontiersin(url):
-    return {"title": "Frontiersin parser (заглушка)", "authors": "—", "journal": "Frontiersin", "issued": "—"}
-
-def parse_routledge(url):
-    return {"title": "Routledge parser (заглушка)", "authors": "—", "journal": "Routledge", "issued": "—"}
-
-def parse_wiley(url):
-    return {"title": "wiley parser (заглушка)", "authors": "—", "journal": "wiley", "issued": "—"}
-
-def parse_jstor(url):
-    return {"title": "jstor parser (заглушка)", "authors": "—", "journal": "jstor", "issued": "—"}
-
-def parse_muse(url):
-    return {"title": "Muse parser (заглушка)", "authors": "—", "journal": "Muse", "issued": "—"}
-
-def parse_crossref(url):
-    return {"title": "crossref parser (заглушка)", "authors": "—", "journal": "crossref", "issued": "—"}
-
-def parse_scholar(url):
-    return {"title": "scholar parser (заглушка)", "authors": "—", "journal": "scholar", "issued": "—"}
-
-def parse_doaj(url):
-    return {"title": "Doaj parser (заглушка)", "authors": "—", "journal": "Doaj", "issued": "—"}
-
-def parse_pubmed(url):
-    return {"title": "Pubmed parser (заглушка)", "authors": "—", "journal": "Pubmed", "issued": "—"}
-
-def parse_ijirmf(url):
-    return {"title": "Ijirmf parser (заглушка)", "authors": "—", "journal": "Ijirmf", "issued": "—"}
-
-def parse_eric(url):
-    return {"title": "Eric parser (заглушка)", "authors": "—", "journal": "Eric", "issued": "—"}
-
-def parse_ieee(url):
-    return {"title": "Ieee parser (заглушка)", "authors": "—", "journal": "Ieee", "issued": "—"}
-
-def parse_acm(url):
-    return {"title": "acm parser (заглушка)", "authors": "—", "journal": "acm", "issued": "—"}
-
-def parse_ssrn(url):
-    return {"title": "ssrn parser (заглушка)", "authors": "—", "journal": "ssrn", "issued": "—"}
-
+def parse_springer(url): return {"title": "Springer parser", "authors": "—", "journal": "Springer", "issued": "—"}
+def parse_sciencedirect(url): return {"title": "Sciencedirect parser", "authors": "—", "journal": "Sciencedirect", "issued": "—"}
+def parse_sagepub(url): return {"title": "Sagepub parser", "authors": "—", "journal": "Sagepub", "issued": "—"}
+def parse_tandfonline(url): return {"title": "Tandfonline parser", "authors": "—", "journal": "Tandfonline", "issued": "—"}
+def parse_bmcmedicine(url): return {"title": "Bmcmedicine parser" "authors": "—", "journal": "Bmcmedicine", "issued": "—"}
+def parse_frontiersin(url): return {"title": "Frontiersin parser", "authors": "—", "journal": "Frontiersin", "issued": "—"}
+def parse_routledge(url): return {"title": "Routledge parser", "authors": "—", "journal": "Routledge", "issued": "—"}
+def parse_wiley(url): return {"title": "wiley parser", "authors": "—", "journal": "wiley", "issued": "—"}
+def parse_jstor(url): return {"title": "jstor parser", "authors": "—", "journal": "jstor", "issued": "—"}
+def parse_muse(url): return {"title": "Muse parser", "authors": "—", "journal": "Muse", "issued": "—"}
+def parse_crossref(url): return {"title": "crossref parser", "authors": "—", "journal": "crossref", "issued": "—"}
+def parse_scholar(url): return {"title": "scholar parser", "authors": "—", "journal": "scholar", "issued": "—"}
+def parse_doaj(url): return {"title": "Doaj parser", "authors": "—", "journal": "Doaj", "issued": "—"}
+def parse_pubmed(url): return {"title": "Pubmed parser", "authors": "—", "journal": "Pubmed", "issued": "—"}
+def parse_ijirmf(url): return {"title": "Ijirmf parser", "authors": "—", "journal": "Ijirmf", "issued": "—"}
+def parse_eric(url): return {"title": "Eric parser", "authors": "—", "journal": "Eric", "issued": "—"}
+def parse_ieee(url): return {"title": "Ieee parser", "authors": "—", "journal": "Ieee", "issued": "—"}
+def parse_acm(url): return {"title": "acm parser", "authors": "—", "journal": "acm", "issued": "—"}
+def parse_ssrn(url): return {"title": "ssrn parser", "authors": "—", "journal": "ssrn", "issued": "—"}
 
 SITES = {
     "www.mdpi.com": parse_mdpi,
