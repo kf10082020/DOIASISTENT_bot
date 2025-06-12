37f8a185b9db7cc3635beed4e2b96f1f6c0b9ee8
kf10082020 <kf10082020@gmail.com>
2025-06-12 13:46:32 +0300

    Add files via upload

--- formatter.py
+++ formatter.py
@@ -1,15 +1,7 @@
-def format_reply(data):
-    if "error" in data:
-        return data["error"]
-    return f"Название: {data.get('title', '—')}
-" \
-           f"Авторы: {data.get('authors', '—')}
-" \
-           f"Год: {data.get('issued', '—')}
-" \
-           f"Журнал: {data.get('journal', '—')}
 
-" \
-           f"Аннотация:
-{data.get('abstract', 'Нет аннотации')}
-"
+def format_reply(metadata: dict) -> str:
+    return f"*Название:* {metadata['title']}\n" \
+           f"*Авторы:* {", ".join(metadata['authors'])}\n" \
+           f"*Журнал:* {metadata['journal']}\n" \
+           f"*Год:* {metadata['year']}\n" \
+           f"*DOI:* {metadata['doi']}"
