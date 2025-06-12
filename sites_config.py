115bdf127b5b7a4177ea62245acd7cfe49cac3dd
kf10082020 <kf10082020@gmail.com>
2025-06-12 17:55:05 +0300

    Update sites_config.py

--- sites_config.py
+++ sites_config.py
@@ -1,5 +1,4 @@
-def parse_mdpi(url):
-    return {"title": "MDPI parser (заглушка)", "authors": "—", "journal": "MDPI", "issued": "—"}
+def parse_mdpi(url): return {"title": "MDPI parser", "authors": "—", "journal": "MDPI", "issued": "—"}
 
 def parse_springer(url):
     return {"title": "Springer parser (заглушка)", "authors": "—", "journal": "Springer", "issued": "—"}
