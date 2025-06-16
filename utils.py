5c10588e9fdd78115f25025252d36b72793139f5
kf10082020 <kf10082020@gmail.com>
2025-06-16 12:11:01 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,6 @@
-﻿Хранит секреты: TELEGRAM_BOT_TOKEN
+import re
+
+def extract_doi(text):
+    """Извлекает DOI из текста сообщения"""
+    match = re.findall(r"10\.\d{4,9}/[-._;()/:A-Z0-9]+", text, re.I)
+    return match[0] if match else None
