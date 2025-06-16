5b0ae3eb02e370cb9c3d50cd4d2e4de5dead50fc
kf10082020 <kf10082020@gmail.com>
2025-06-16 12:08:47 +0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,7 +1,1 @@
-import re
-
-def extract_doi(text: str) -> str or None:
-    # Простая регулярка для DOI (можно улучшить)
-    pattern = r"10.\d{4,9}/[-._;()/:A-Z0-9]+"
-    matches = re.findall(pattern, text, flags=re.I)
-    return matches[0] if matches else None
+﻿Хранит секреты: TELEGRAM_BOT_TOKEN
